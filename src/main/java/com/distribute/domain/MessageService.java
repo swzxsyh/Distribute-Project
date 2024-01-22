bd1d1cb866c23eb9254e73eb43f31aad4b67f0fe
swzxsyh <swzxsyh@users.noreply.github.com>
2024-01-22 11:12:05 +0800

    Update MessageService.java
    web socket节点数据分发

--- src/main/java/com/distribute/domain/MessageService.java
+++ src/main/java/com/distribute/domain/MessageService.java
@@ -15,23 +15,38 @@ public class MessageService {
     @Autowired
     private WebSocketServer webSocketServer;
 
+    @Autowired
+    private RedissonClient client;
 
-//    @Autowired
-//    private RedisTemp redisTemplate;
-
-
-    public void send(String message) {
-//        redisTemplate.addListener((channel, message1) -> {
-//            log.info("channel: {}, message: {}", channel, message1);
-//        }
-
-
-        // 利用redis channel publish发送消息到所有节点
-        webSocketServer.send(message);
-    }
+    static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1, new BasicThreadFactory.Builder().namingPattern("WebSocketServer-reconnect-schedule-pool-%d").daemon(true).build());
 
     @PostConstruct
-    public void consume() {
-        // 利用redis channel 消费消息 subscribe
+    public <T> void config() {
+        log.info("WsSubscriber-init");
+        // 订阅消息 channel type, consumer
+        executor.execute(() -> this.subscribe(CacheConstant.WEBSOCKET_MESSAGE_CHANNEL, WebSocketParam.class, channelMessage -> {
+            log.info("CasinoMessageService-收到消息: {}", channelMessage);
+            Object pushData = channelMessage.getData();
+            String message = channelMessage.getMessage();
+
+           webSocketServer.send(message);
+        }));
     }
+
+
+    public static <T> void publish(String channelKey, T msg) {
+        RTopic topic = client.getTopic(channelKey);
+        topic.publish(msg);
+    }
+
+    public static <T> void subscribe(String channelKey, Class<T> clazz, Consumer<T> consumer) {
+        RTopic topic = client.getTopic(channelKey);
+        topic.addListener(clazz, (channel, msg) -> consumer.accept(msg));
+    }
+
+    public void send(String message) {
+        // 利用redis channel publish发送消息到所有节点
+        this.publish(CacheConstant.WEBSOCKET_MESSAGE_CHANNEL,message)
+    }
+
 }
