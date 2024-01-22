6fba7e58038c86fec5d9272daa9a1fa61e075972
swzxsyh <swzxsyh@users.noreply.github.com>
2024-01-22 10:53:52 +0800

    Update JsonUtil.java
    添加JSON转换方法

--- src/main/java/com/distribute/persistence/util/JsonUtil.java
+++ src/main/java/com/distribute/persistence/util/JsonUtil.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 
+/*
+* Json工具类, 转换为指定数据
+*/
 @Slf4j
 public class JsonUtil {
 
@@ -43,6 +46,27 @@ public class JsonUtil {
             return objectMapper.writeValueAsString(param);
         } catch (Exception e) {
             log.error("json转换异常", e);
+        }
+        return null;
+    }
+
+    public static <T> List<T> toList(String json, Class<T> clazz) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+            CollectionType collectionType = mapper.getTypeFactory().constructCollectionType(List.class, clazz);
+            return mapper.readValue(json, collectionType);
+        } catch (Exception e) {
+            log.error("json序列化异常, json:{} ,e:", json, e);
+        }
+        return null;
+    }
+
+    public static <T> T findValue(String json, String fieldName, Class<T> clazz) {
+        try {
+            return objectMapper.readTree(json).findValue(fieldName).traverse(new ObjectMapper()).readValueAs(clazz);
+        } catch (Exception e) {
+            log.error("json序列化异常:", e);
         }
         return null;
     }
